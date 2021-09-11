77a8689d2c5c0d743d05afb76d93600ab4037662
Sandeep Singh <sandeepsingh96082@gmail.com>
2021-09-11 22:11:13 +0530

    Improvement in the app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import About from "./components/About";
+import { useState } from "react";
+import Alert from "./components/Alert";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+const App = () => {
+    const [mode, setMode] = useState('light')
+    const [alert, setAlert] = useState(null)
+
+    const showAlert = (message, type) => {
+        setAlert({
+            msg: message,
+            type: type
+        })
+        setTimeout(() => {
+            setAlert(null)
+        }, 1500)
+    }
+
+    const toggle = () => {
+        if (mode === 'light') {
+            setMode('dark')
+            document.body.style.backgroundColor = '#03203C'
+            showAlert("Dark Mode has been enabled", "success")
+        } else {
+            setMode('light')
+            document.body.style.backgroundColor = 'white'
+            showAlert("Light Mode has been enabled", "success")
+        }
+    }
+
+
+    return (
+
+        <Router>
+            <Navbar title="Analyze Your Text" tab1="Home" tab2="About" mode={mode} toggle={toggle} />
+            <Alert alert={alert} />
+            <Switch>
+                <Route exact path="/">
+                    <TextForm showAlert={showAlert} heading="Enter your text to analyze" mode={mode} />
+                </Route>
+
+                <Route exact path="/about">
+                    <About />
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,47 @@
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+const Navbar = (props) => {
+    return (
+        <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="/">{props.title}</Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link className="nav-link active" aria-current="page" to="/">{props.tab1}</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/about">{props.tab2}</Link>
+                        </li>
+                    </ul>
+                    {/* <form className="d-flex">
+                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                        <button className="btn btn-outline-primary" type="submit">Search</button>
+                    </form> */}
+
+                    <div className="form-check form-switch mx-5">
+                        <input onClick={props.toggle} className="form-check-input" type="checkbox" id="flexSwitchCheckDefault" />
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+Navbar.propTypes = {
+    title: PropTypes.string.isRequired,
+    tab1: PropTypes.string,
+    tab2: PropTypes.string
+}
+
+Navbar.defaultProps = {
+    title: 'Analyze Your Text',
+    tab1: 'Home',
+    tab2: 'About'
+}
+
+export default Navbar;
